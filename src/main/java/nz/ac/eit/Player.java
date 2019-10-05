536fcc520a2e5ff8408cfa127f55f1d99dfd523c
FloydWatson <floydbcseit@gmail.com>
2019-10-05 20:28:07 +1300

    created player

---
+++ src/main/java/nz/ac/eit/Player.java
@@ -0,0 +1,18 @@
+package nz.ac.eit;
+
+public class Player {
+
+    private int handValue = 0;
+
+    public Player(){}
+
+    public int getHandValue(){
+        return handValue;
+    }
+
+    public void setHandValue(int value){
+        handValue = value;
+    }
+
+
+}
